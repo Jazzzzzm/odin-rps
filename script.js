7b331a7111b5f8f6bc8066a2dc76fe761c2c04e8
Jasmine Vu <jvu45@uwo.ca>
2025-07-01 16:41:10 -0400

    Added function that generates 1 of 3 options from rock, paper, and scissors using Math.random

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+/* Testing connection
 console.log("Hello World");
+*/
+
+function getComputerChoice(){
+    let rand_num = Math.random()*3;
+    if(0 < rand_num && rand_num <= 1) {return "rock"}
+    else if(1 < rand_num && rand_num <= 2) {return "paper"}
+    else {return "scissors"}
+}
+
+/* Testing getComputerChoice() function */
+console.log(getComputerChoice());
